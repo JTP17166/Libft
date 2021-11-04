3f25170c0779d49a4536c7d84142a052e8143765
João Tomás Pereira <93710591+JTP17166@users.noreply.github.com>
2021-11-04 12:06:06 +0000

    Update ft_isascii.c

--- Libft/ft_isascii.c
+++ Libft/ft_isascii.c
@@ -17,6 +17,6 @@ int	ft_isascii(int c)
 	return ((c >= 0) && (c <= 127));
 }
 /*
-* The isascii() function tests for an ASCII character, which is any character
+* L15 -> The isascii() function tests for an ASCII character, which is any character
 * between 0 and octal 0177 inclusive.
 */
