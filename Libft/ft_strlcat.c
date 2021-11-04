8835012df8de7fe432728a75caba5b19e7e43eeb
João Tomás Pereira <93710591+JTP17166@users.noreply.github.com>
2021-11-04 18:53:38 +0000

    Update ft_strlcat.c

--- Libft/ft_strlcat.c
+++ Libft/ft_strlcat.c
@@ -39,13 +39,13 @@ size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 }
 
 /*
- * L17 -> strlcat() appends string src to the end of dst.
- * It will append at most dstsize - strlen(dst) - 1 characters.
- * It will then NUL-terminate, unless dstsize is 0 
- * or the original dst string was longer than dstsize 
- * (in practice this should not happen as it means that either
- * dstsize is incorrect or that dst is not a proper string).
- * L22 -> strlen, strnlen -- find length of string
- * L28 ->  strlen, strnlen -- find length of string
- * L38 ->  strlen, strnlen -- find length of string
+* L17 -> strlcat() appends string src to the end of dst.
+* It will append at most dstsize - strlen(dst) - 1 characters.
+* It will then NUL-terminate, unless dstsize is 0 
+* or the original dst string was longer than dstsize 
+* (in practice this should not happen as it means that either
+* dstsize is incorrect or that dst is not a proper string).
+* L22 -> strlen -- find length of string
+* L28 ->  strlen -- find length of string
+* L38 ->  strlen -- find length of string
 */
