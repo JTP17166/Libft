66010a7d17c2ea2fcea899a9f3dfd33bb3a41b58
João Tomás Pereira <93710591+JTP17166@users.noreply.github.com>
2021-11-04 12:10:47 +0000

    Update ft_isdigit.c

--- Libft/ft_isdigit.c
+++ Libft/ft_isdigit.c
@@ -25,9 +25,7 @@ int	ft_isdigit(int c)
 }
 
 /*
-* The isnumber() function behaves similarly to isdigit(), 
-* but may recognize additional characters, depending on the current 
-* locale setting.
-* The value of the argument must be representable as an unsigned char or the
-* value of EOF.
+* L15 -> The isdigit() function tests for a decimal digit character.
+* Regardless of locale, this includes the following characters
+* only: (0 to 9).
 */
